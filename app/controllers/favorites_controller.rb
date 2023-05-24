7533c6c26557742fdd6b70bf4a6ec2c8d2191b18
junho <cclemon3248@gmail.com>
2023-05-25 03:29:35 +0900

    [add]お気に入りページを作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,9 @@
 class FavoritesController < ApplicationController
+  def index
+    @blogs = current_user.favorite_blogs.where(ruby_rails: "Ruby")
+    @rails = current_user.favorite_blogs.where(ruby_rails: "Rails")
+  end
+  
   def create
     blog = Blog.find(params[:blog_id])
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
@@ -6,9 +11,9 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    blog_id = current_user.favorites.find_by(id: params[:id]).blog_id
-    blog = Blog.find(blog_id)
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    favorite = current_user.favorites.find_by(id: params[:id])
+    blog = favorite.blog
+    favorite.destroy
     redirect_to blog, notice: "お気に入り解除しました"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/home/ruby', to: 'home#ruby'
+  get '/home/rails', to: 'home#rails'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :blogs do
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
 end
