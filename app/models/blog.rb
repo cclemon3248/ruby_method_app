de306afd6a7dc3544a76b1290024e948b5705a66
junho <cclemon3248@gmail.com>
2023-07-05 03:18:36 +0900

    [update]メソッドの記事の文字制限を設定。

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,11 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 30 }
   validates :ruby_rails, presence: true
+  validates :content, presence: true, length: { maximum: 320 }
+  validates :code1, presence: true, length: { maximum: 1600 }
+  validates :code2, presence: true, length: { maximum: 1600 }
+  validates :code3, length: { maximum: 1600 }
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   has_many :comprehensions, dependent: :destroy
